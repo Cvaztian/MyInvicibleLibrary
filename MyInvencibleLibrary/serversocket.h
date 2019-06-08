48703dac9c33de1d78ffee7d83489ab475bdfbff
Kevin <magneticok@gmail.com>
2019-06-07 20:04:57 -0600

    Base de comunicacion socket
    Se agrega la clase encargada de manejar los sockets, asi como el prototipo de las funciones enviar y recibir.

---
+++ MyInvencibleLibrary/serversocket.h
@@ -0,0 +1,21 @@
+#ifndef SERVERSOCKET_H
+#define SERVERSOCKET_H
+
+/** @brief Clase encargada de la conexion por sockets
+ * Clase encargada de la conexion mediante sockets. La progra esta distribuida en 4
+ * librerias, 3 principales y una de conexion, las tres principales se comunican
+ * con las demas mediante la de conexion por medio de sockets.
+ * @author Elorim
+ * @date 07/07/19
+*/
+class ServerSocket
+{
+public:
+    /**
+      Constructor default, encargado de inicializar el servidor.
+      @author Elorim
+*/
+    ServerSocket();
+};
+
+#endif // SERVERSOCKET_H
