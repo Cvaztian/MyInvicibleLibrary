d90b18ae99a4a3d0db731a9d5c728ae93bb4e1aa
Kevin <magneticok@gmail.com>
2019-06-09 00:02:46 -0600

    Pruebas con archivos y carpetas

--- MyInvencibleLibrary/fiomain.cpp
+++ MyInvencibleLibrary/fiomain.cpp
@@ -1,12 +1,62 @@
 // basic file operations
 #include <iostream>
 #include <fstream>
+#include <bits/stdc++.h>
+#include <iostream>
+#include <sys/stat.h>
+#include <sys/types.h>
+#include <stdio.h>
+#include <iostream>
+#include <fstream>
+#include <string>
+#include "nlohmann/json.hpp"
+
 using namespace std;
 
 int main () {
-  ofstream myfile;
-  myfile.open ("example.txt");
-  myfile << "Writing this to a filesss.\n";  // Esto crea un nuevo archivo y reescribe todo lo que hay en el
-  myfile.close();
+    // Creating a directory
+        if (mkdir("a", 0777) == -1)
+            cerr << "Error :  " << strerror(errno) << endl;  // Si ya existe
+        else
+            cout << "Directory created";  // Si no existe
+
+        // Verificando si archivo existe
+        ifstream f("a/example.txt");
+        f.good();
+        cout << f.good() << endl;  // 1 de existir, 0 si no.
+
+        // Escribiendo en archivo o creando.
+        nlohmann::json j;
+        j = {{"id",1},{"Imagen","123"}};  // Al escribir las cosas en json no hay espacios.
+        ofstream myfile;
+        myfile.open ("a/example.txt");  // Se puede especificar una carpeta, si existe, crea el archivo dentro, si no, no hace nada.
+        myfile << j;  // Esto crea un nuevo archivo y reescribe todo lo que hay en el
+        myfile.close();
+
+        // Leyendo
+        string data;
+        ifstream infile;
+           infile.open("a/example.txt");
+
+           cout << "Reading from the file" << endl;
+           infile >> data;  // Lee linea por linea
+
+           // write the data at the screen.
+           cout << data << endl;
+
+           // Extrayendo json
+           nlohmann::json js = nlohmann::json::parse(data);
+           cout << js["id"] <<endl;  // json funciona
+
+           // close the opened file.
+           infile.close();
+
+        // Eliminando archivo
+        if (remove("example.txt") == 0)
+           printf("Deleted successfully"); // Existe
+        else
+           printf("Unable to delete the file");  // No existe
+
+
   return 0;
 }
